b30322ddb477a34f78d4c6cd943e012a0b964da1
Paul Unsted <ed__84@hotmail.com>
2020-05-07 22:05:42 -0400

    Came across a bug while commenting-out some logic: if you add the same item to your cart multiple times, it creates an individual db entry each time. This creates problems when trying to remove these items since they share foreign keys. I'll have to parse through the db before population in order to make certain that the item doesn't already exist, and if it does run an update instead of an insert.

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,30 +1,39 @@
+// takes items from main db and places them in the cart
 $(".wrap").on("submit", function (event) {
   event.preventDefault();
+  // get the data needed and make an object to hold it
   let amt = $(this).context.elements[0].value;
   let inv = $(this).data("inv");
   let grocery = {
     id: $(this).data("id"),
     amt: amt,
   };
+  // post item(s) to cart
   $.ajax("/api/popCart", {
     type: "POST",
     data: grocery,
   });
+  // remove that amount of items from db
   grocery.amt = inv - amt;
   $.ajax("/api/updateGroceries", {
     type: "POST",
     data: grocery,
+    // and then reload page
   }).then(function () {
     location.reload();
   });
 });
 
+// removes items from cart and inserts them back into main db
 $(".cartItem").on("submit", function (event) {
   event.preventDefault();
+  // get the data needed
   let amt = parseInt($(this).context.elements[0].value);
   let id = parseInt(this.id);
   let cartInv = parseInt($(this).context.elements[0].max);
   let newInv = null;
+
+  // find the item that corresponds with the one you're removing, adjust the value as needed and store it
   for (let i = 0; i < inventory.length; i++) {
     if (inventory[i].id === id) {
       newInv = amt + inventory[i].inventory;
@@ -34,11 +43,13 @@ $(".cartItem").on("submit", function (event) {
     id: id,
     amt: newInv,
   };
+  // repopulate main db
   $.ajax("/api/updateGroceries", {
     type: "POST",
     data: grocery,
   });
   grocery.amt = cartInv - amt;
+  // checks to see if the number of items being removed from the cart is the total number of that item in the cart, and if so removes that row from the cart db
   if (amt === cartInv) {
     $.ajax("/api/removeItem", {
       type: "POST",
@@ -46,19 +57,25 @@ $(".cartItem").on("submit", function (event) {
     }).then(function () {
       location.reload();
     });
-  } else {
+  }
+  // otherwise adjust the amt and update the db
+  else {
     $.ajax("/api/updateCart", {
       type: "POST",
       data: grocery,
+      // and then reload page
     }).then(function () {
       location.reload();
     });
   }
 });
 
+// purges cart db entirely
+// need to add main db repopulation to this function
 $("#purge").on("click", function (event) {
   event.preventDefault();
   console.log("Purge running");
+  // insert repop code here, thinking of a for loop that will iterate through all cart items and run /api/updateCart for each of them before purging cart.
   $.ajax("/api/purge", {
     type: "POST",
   }).then(function () {
