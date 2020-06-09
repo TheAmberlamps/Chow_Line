85a6dedf509614bb1c0ca2458032f4d578cf3491
Paul Unsted <ed__84@hotmail.com>
2020-06-08 23:40:29 -0400

    Seem to have identified my issue, commented-out some thoughts about how to proceed, going to merge branch into master.

--- models/grocery.js
+++ models/grocery.js
@@ -1,5 +1,9 @@
 var orm = require("../config/orm.js");
 
+// OK, so the fact that this file isn't exported as a function means that sequelize is throwing a fit and the server won't initialize. The problem may be deeper than that but I won't be able to tell until that's sorted.
+
+// Also, given their absence in Spartan Meet it looks as though I'm going to need to remove orm.js and connection.js. My MVC paradigm sincerely doesn't seem to want to play with sequelize, this may require some serious surgery.
+
 var grocery = {
   selectAllUsers: function (cb) {
     orm.selectAll("users", function (res) {
